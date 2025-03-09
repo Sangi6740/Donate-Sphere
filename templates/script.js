ff5db22263c6d29712cf18e97ee63414f4bc033b
Sangi6740 <158066794+Sangi6740@users.noreply.github.com>
2025-03-09 17:12:33 +0530

    Add files via upload

---
+++ templates/script.js
@@ -0,0 +1,6 @@
+function validation() {
+        // Show the success message after form submission
+        document.getElementById("success-message").style.display = "block";
+        return true; // Return true to allow form submission
+    }
+    
